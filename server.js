ec16dcf5b2ca768b4550a06481e33bfe0929ea4b
Lauren Bleyl <laurenbleyl@gmail.com>
2023-08-11 00:18:31 -0500

    finished i think

--- server.js
+++ server.js
@@ -3,11 +3,6 @@
 
 const inquirer = require('inquirer');
 const connection = require("./config/connection");
-// const { viewAllDepartments, viewAllRoles, viewAllEmployees } = require('./lib/views')
-// const { addDepartment, addRole, addEmployee, updateRole } = require ('./lib/queries')
-
-
-
 
 
 connection.connect (err => {
@@ -59,9 +54,9 @@ function startPrompt() {
       if (choices === "Add a role") {
         addRole();
       }
-      if (choices === "Add an employee") {
-        addEmployee();
-      }
+      // if (choices === "Add an employee") {
+      //   addEmployee();
+      // }
       if (choices === "Update employee role") {
         updateRole();
       }
@@ -122,6 +117,17 @@ function addDepartment() {
 }
 
 function addRole() {
+  const allDepartments = []
+  connection.query('SELECT * FROM department', (err, res) => {
+    if (err) throw err
+    res.forEach(dept=> {
+      let deptList = {
+        name: dept.name,
+        value: dept.id
+      }
+      allDepartments.push(deptList)
+    })
+  })
   inquirer.prompt ([
     {
       type: 'input',
@@ -132,25 +138,94 @@ function addRole() {
       type: "input",
       name: "newSalary",
       message: "What is the salary of the new role?"
+    },
+    {
+      type: 'list',
+      name: 'newRoleDept',
+      choices: allDepartments,
+      message: "What is the role's department?"
     }
   ])
+  .then(response => {
+    connection.query('INSERT INTO roles (title, salary, department_id) VALUES (?)',
+    [[response.newRole, response.newSalary, response.newRoleDept]], (err,res) => {
+      if (err) throw err
+      console.log(`Added ${response.newRole} to roles.`)
+      viewAllRoles()
+    })
+  })
 }
 
-function addEmployee() {
-  inquirer.prompt ([
-    {
-      type: 'input',
-      name: "newEmployeeFirst",
-      message: "What is the first name of the employee?"
-    },
-    {
-      type: 'input',
-      name: "newEmployeeLast",
-      message: "What is the last name of the employee?"
-    },
-  ])
-}
 
 function updateRole() {
-
+  connection.query('SELECT * FROM employee', (err, res) => {
+    if (err) throw err
+    const allEmployees = []
+    res.forEach(({ first_name, id}) => {
+      allEmployees.push({
+        name: first_name,
+        value: id
+      })
+    })
+    connection.query('SELECT * FROM roles', (err, res) => {
+      if (err) throw err
+      const allRoles = []
+      res.forEach(({ title, id }) => {
+        allRoles.push({
+          name: title,
+          value: id
+        })
+      })
+      inquirer.prompt([
+          {
+            type: 'list',
+            name: 'employees',
+            choices: allEmployees,
+            message: "What employee would you like to update?"
+          },
+          {
+            type: 'list',
+            name: 'roles',
+            choices: allRoles,
+            message: "What is the employee's new role?"
+          }
+      ])
+      .then(response => {
+        connection.query('UPDATE employee SET ? Where ?? = ?', 
+        [{role_id: response.role_id}, 'id', response.id]
+        , (err, res) => {
+          if (err) throw err
+          console.log(`Updated employee's role.`)
+          viewAllEmployees()
+        })
+      })
+    })
+  })
 }
+
+// function addEmployee() {
+//   inquirer.prompt ([
+//     {
+//       type: 'input',
+//       name: "newEmployeeFirst",
+//       message: "What is the first name of the employee?"
+//     },
+//     {
+//       type: 'input',
+//       name: "newEmployeeLast",
+//       message: "What is the last name of the employee?"
+//     },
+//     {
+//       type: 'list',
+//       name: 'newRoleEmployee',
+//       choices: '', 
+//       message: "What is the employee's role?"
+//     },
+//     {
+//       type: 'list',
+//       name: 'newManagerEmployee',
+//       choices:'',
+//       message: "Who is the employee's manager?"
+//     }
+//   ])
+// }
